2dce539f09799c74f4831a578dc99c5d14b84b56
Keegan McAllister <mcallister.keegan@gmail.com>
2010-08-11 06:56:35 -0400

    simplify and document

--- jvflife.c
+++ jvflife.c
@@ -1,10 +1,18 @@
+// jvflife.c
+// Game of Life for the JVF 2010-A LED display
+//
+// Written by Keegan McAllister <keegan _at_ t0rch.org>
+//
+// I release this code into the public domain with
+// no restrictions whatsoever.
+
 #include <conio.h>
-#include <graph.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 #include <i86.h>
+
+
+// JVF 2010-A: 128x48 pixels
 
 #define GRID_X 128
 #define GRID_Y  48
@@ -19,6 +27,9 @@ byte rand_byte() {
   return rand() >> 8;
 }
 
+
+// Grid of cells.  We read from one buffer
+// while updating the other.
 val  grid_a[GRID_Y*GRID_X];
 val  grid_b[GRID_Y*GRID_X];
 val* grid_r = grid_a;
@@ -27,95 +38,22 @@ val* grid_w = grid_b;
 #define IDX(x,y) ((y)*GRID_X + (x))
 
 
-
-// Define for a CGA graphics simulation;
-// undef  to draw to real LED hardware.
-#undef DRAW_CGA
-
-#ifdef DRAW_CGA
+////////////////////////////////////////////////////////////
+// Interfacing to the LED hardware
+////////////////////////////////////////////////////////////
 
 
-
-// CGA 320x200 interlaced mode
-// 80 bytes per scanline
-// 4 pixels per byte
-// grid starts 52 pixels = 26 scanline pairs from top
-// grid starts 40 pixels = 20 bytes from left
-// draw 2x2 grid of pixels for one LED
-//   => half of one byte, on each of a pair of lines
-
-#define SCANLINE 80
-#define BUF_HALF_SIZE (GRID_Y*SCANLINE)
-#define BUF_HALF_OFF  (26*SCANLINE)
-
-byte vmem_buf[BUF_HALF_SIZE*2];
-byte far* vmem = (byte far*) 0xB8000000L;
-#define ILACE_OFF 0x2000
-
-void graphics_init() {
-  _setvideomode(_MRES4COLOR);
-
-  _rectangle(_GFILLINTERIOR,  29,  42, 306,  49);
-  _rectangle(_GFILLINTERIOR,  29, 150, 306, 157);
-  _rectangle(_GFILLINTERIOR,  29,  50,  36, 149);
-  _rectangle(_GFILLINTERIOR, 299,  50, 306, 149);
-
-  _fmemcpy(&vmem_buf[0],
-           vmem + BUF_HALF_OFF,
-           BUF_HALF_SIZE);
-  _fmemcpy(&vmem_buf[BUF_HALF_SIZE],
-           vmem + ILACE_OFF + BUF_HALF_OFF,
-           BUF_HALF_SIZE);
-}
-
-void graphics_update() {
-  _fmemcpy(vmem + BUF_HALF_OFF,
-           &vmem_buf[0],
-           BUF_HALF_SIZE);
-  _fmemcpy(vmem + ILACE_OFF + BUF_HALF_OFF,
-           &vmem_buf[BUF_HALF_SIZE],
-           BUF_HALF_SIZE);
-}
-
-void graphics_finalize() {
-  _setvideomode(_DEFAULTMODE);
-}
-
-void grid_set(idx x, idx y, val v) {
-  unsigned short off;
-  byte mask;
-
-  v = v ? 1 : 0;
-
-  grid_w[IDX(x,y)] = v;
-
-  off = SCANLINE*y + 10 + (x >> 1);
-  mask = (x&1) ? 0x0F : 0xF0;
-  if (v) {
-    vmem_buf[off] |= mask;
-    vmem_buf[off+BUF_HALF_SIZE] |= mask;
-  } else {
-    mask ^= 0xFF;
-    vmem_buf[off] &= mask;
-    vmem_buf[off+BUF_HALF_SIZE] &= mask;
-  }
-}
-
-
-
-#else
-
-
-
-// Draw to the real LED hardware
-
+// 4 pixels per byte = 2 bits per pixel
+// This allows 4-level greyscale, but we use monochrome only.
 #define SCANLINE (GRID_X/4)
 #define BUF_SIZE (GRID_Y*SCANLINE)
 
-#define CONFIG_PORT 0x02f0
-
 byte led_buf[BUF_SIZE];
 
+// We communicate with the LED hardware on this I/O port.
+#define CONFIG_PORT 0x02f0
+
+// Parameters for DMA transmission.
 byte dma_chan;
 byte dma_mode;
 byte dma_addr2_port;
@@ -124,9 +62,11 @@ byte dma_done_mask;
 byte dma_base_port;
 byte dma_count_port;
 
-void graphics_init() {
+// Initialize hardware and read DMA parameters.
+void led_init() {
   byte config_byte = inp(CONFIG_PORT);
 
+  // bit 0x40 tells us the DMA channel
   dma_chan = (config_byte & 0x40) ? 3 : 1;
 
   if (dma_chan == 3) {
@@ -144,76 +84,105 @@ void graphics_init() {
     dma_base_port  = 2;
     dma_count_port = 3;
   }
-
-  //printf("Initialized DMA on channel %d\n", dma_chan);
 }
 
-void graphics_finalize() { }
-
+// Ports used to communicate with the first
+// DMA controller, regardless of channel.
 #define DMA0_STATUS  0x08
 #define DMA0_MASK    0x0A
 #define DMA0_MODE    0x0B
 #define DMA0_COUNTER 0x0C
 
+// Write a line of pixels from a given starting address
+// to the LED hardware.
 void dma_write(byte far* line) {
   unsigned int seg, off, addr, count;
 
-  outp(DMA0_MODE,    dma_mode);
-  outp(DMA0_COUNTER, 0xFF    );
+  // set the DMA mode
+  outp(DMA0_MODE, dma_mode);
 
+  // reset counter; value is ignored
+  outp(DMA0_COUNTER, 0xFF);
+
+  // break the far pointer into segment and offset components
   seg = FP_SEG(line);
   off = FP_OFF(line);
 
+  // compute the low 16 (of 20) bits of the *physical* address
   addr = ((seg & 0x0FFF) << 4) + off;
+
+  // write the 20-bit physical address to the DMA controller
   outp(dma_base_port ,  addr &   0xFF);
   outp(dma_base_port , (addr & 0xFF00) >>  8);
   outp(dma_addr2_port, (seg  & 0xF000) >> 12);
 
+  // counter is an inclusive upper bound, so write one less than
+  // the number of bytes to transfer
   count = SCANLINE-1;
   outp(dma_count_port,  count &   0xFF);
   outp(dma_count_port, (count & 0xFF00) >> 8);
 
+  // set the mask, commencing transmission
   outp(DMA0_MASK, dma_mask);
 
+  // tell the LED hardware to begin reading?
+  // exact purpose unclear
   outp(CONFIG_PORT, 0x8f);
   outp(CONFIG_PORT, 0x0f);
   outp(CONFIG_PORT,    7);
 }
 
+// Check whether DMA is finished.
 inline byte dma_finished() {
   byte v = inp(DMA0_STATUS);
   return (v & dma_done_mask);
 }
 
-inline void lineaddr_reset() {
+// The LED hardware keeps track of which line 
+// to set with the next DMA transfer.
+
+// Reset to the first line.
+inline void line_reset() {
   outp(CONFIG_PORT, 5);
   outp(CONFIG_PORT, 7);
 }
 
-inline void lineaddr_incr() {
+// Increment to the next line.
+inline void line_incr() {
   outp(CONFIG_PORT, 6);
   outp(CONFIG_PORT, 7);
 }
 
-void graphics_update() {
+// Write all lines to the LED hardware.
+void led_update() {
   idx y;
 
-  lineaddr_reset();
+  line_reset();
   for (y=0; y<GRID_Y; y++) {
     dma_write(&led_buf[SCANLINE*y]);
-    while (!dma_finished());
-    lineaddr_incr();
+
+    while (!dma_finished()) { }
+
+    if (y < MAX_Y) line_incr();
   }
 }
 
+
+////////////////////////////////////////////////////////////
+// Computing the cellular automaton
+////////////////////////////////////////////////////////////
+
+
+// Set a pixel in the grid, and in the
+// LED output buffer.
 void grid_set(idx x, idx y, val v) {
   unsigned short off;
   byte mask;
 
-  v = v ? 1 : 0;
+  grid_w[IDX(x,y)] = v ? 1 : 0;
 
-  grid_w[IDX(x,y)] = v;
-
+  // four pixels per byte,
+  // leftmost is most significant
   off = SCANLINE*y + (x >> 2);
   mask = 0xC0 >> ((x&3) << 1);
   if (v) {
@@ -223,20 +192,18 @@ void grid_set(idx x, idx y, val v) {
   }
 }
 
-#endif
-
-
-
-
+// Flip the double-buffered grid, and write
+// to the LED hardware.
 void grid_flip() {
   val* tmp;
   tmp = grid_r;
   grid_r = grid_w;
   grid_w = tmp;
 
-  graphics_update();
+  led_update();
 }
 
+// Initialize the grid with randomness.
 void grid_init() {
   idx x,y;
   for (y=0; y<GRID_Y; y++) {
@@ -247,14 +214,13 @@ void grid_init() {
   grid_flip();
 }
 
-// rule selection
+// Rule selection:
 #define B(n) ((1 << (n)))
-// HighLife
-short born = B(3) | B(6);
-short live = B(2) | B(3);
+short born = B(3) | B(6);  // neighbor-counts where a cell is born
+short live = B(2) | B(3);  // neighbor-counts where a cell survives
+// Above is the HighLife rule.
 
-#define SET_ALIVE(x,y) grid_set((x), (y), (self ? live : born) & B(nghb))
-#define GR(x,y) grid_r[IDX((x),(y))]
+// Occasionally we spawn a glider to keep things interesting.
 
 val glider[9] = { 0, 0, 1, 1, 0, 1, 0, 1, 1 };
 
@@ -264,8 +230,8 @@ void spawn_glider() {
   xo = rand_byte() % (GRID_X-2);
   yo = rand_byte() % (GRID_Y-2);
   refl = rand_byte();
-  rx = refl & 1;
-  ry = refl & 2;
+  rx = refl & 1;  // reflect on x-axis?
+  ry = refl & 2;  // reflect on y-axis?
   for (dy=0; dy<3; dy++) {
     for (dx=0; dx<3; dx++) {
       ex = rx ? (2-dx) : dx;
@@ -275,6 +241,10 @@ void spawn_glider() {
   }
 }
 
+#define SET_ALIVE(x,y) grid_set((x), (y), (self ? live : born) & B(nghb))
+#define GR(x,y) grid_r[IDX((x),(y))]
+
+// One step of the cellular automaton.
 void step() {
   idx x,y,xm,xp,ym,yp;
   val self, nghb;
@@ -352,19 +322,29 @@ void step() {
     }
   }
 
+  // With low probability, spawn a glider.
   if (rand_byte() < 4) spawn_glider();
 
   grid_flip();
 }
 
+
+////////////////////////////////////////////////////////////
+// Entry point
+////////////////////////////////////////////////////////////
+
+
 int main() {
   srand(time(NULL));
-  graphics_init();
+  led_init();
   grid_init();
+
   for (;;) {
     step();
+
+    // Exit on keypress
     if (kbhit()) break;
   }
-  graphics_finalize();
+
   return 0;
 }
