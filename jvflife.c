09e1383a8e94aa1647ce1eff5e02f140c875ca8b
Keegan McAllister <mcallister.keegan@gmail.com>
2010-08-10 22:56:02 -0400

    working DMA to real hardware

--- jvflife.c
+++ jvflife.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <i86.h>
 
 #define GRID_X 128
 #define GRID_Y  48
@@ -13,11 +14,6 @@
 typedef short idx;
 typedef char  val;
 typedef unsigned char byte;
-
-void jvf_init() {
-  inp(0x02F0);
-}
-
 
 byte rand_byte() {
   return rand() >> 8;
@@ -29,6 +25,16 @@ val* grid_r = grid_a;
 val* grid_w = grid_b;
 
 #define IDX(x,y) ((y)*GRID_X + (x))
+
+
+
+// Define for a CGA graphics simulation;
+// undef  to draw to real LED hardware.
+#undef DRAW_CGA
+
+#ifdef DRAW_CGA
+
+
 
 // CGA 320x200 interlaced mode
 // 80 bytes per scanline
@@ -46,7 +52,9 @@ byte vmem_buf[BUF_HALF_SIZE*2];
 byte far* vmem = (byte far*) 0xB8000000L;
 #define ILACE_OFF 0x2000
 
-void draw_border() {
+void graphics_init() {
+  _setvideomode(_MRES4COLOR);
+
   _rectangle(_GFILLINTERIOR,  29,  42, 306,  49);
   _rectangle(_GFILLINTERIOR,  29, 150, 306, 157);
   _rectangle(_GFILLINTERIOR,  29,  50,  36, 149);
@@ -60,12 +68,7 @@ void draw_border() {
            BUF_HALF_SIZE);
 }
 
-void grid_flip() {
-  val* tmp;
-  tmp = grid_r;
-  grid_r = grid_w;
-  grid_w = tmp;
-  
+void graphics_update() {
   _fmemcpy(vmem + BUF_HALF_OFF,
            &vmem_buf[0],
            BUF_HALF_SIZE);
@@ -74,9 +77,15 @@ void grid_flip() {
            BUF_HALF_SIZE);
 }
 
+void graphics_finalize() {
+  _setvideomode(_DEFAULTMODE);
+}
+
 void grid_set(idx x, idx y, val v) {
   unsigned short off;
   byte mask;
+
+  v = v ? 1 : 0;
 
   grid_w[IDX(x,y)] = v;
 
@@ -92,17 +101,159 @@ void grid_set(idx x, idx y, val v) {
   }
 }
 
-void grid_seed() {
+
+
+#else
+
+
+
+// Draw to the real LED hardware
+
+#define SCANLINE (GRID_X/4)
+#define BUF_SIZE (GRID_Y*SCANLINE)
+
+#define CONFIG_PORT 0x02f0
+
+byte led_buf[BUF_SIZE];
+
+byte dma_chan;
+byte dma_mode;
+byte dma_addr2_port;
+byte dma_mask;
+byte dma_done_mask;
+byte dma_base_port;
+byte dma_count_port;
+
+void graphics_init() {
+  byte config_byte = inp(CONFIG_PORT);
+
+  dma_chan = (config_byte & 0x40) ? 3 : 1;
+
+  if (dma_chan == 3) {
+    dma_mode       = 0x4B;
+    dma_addr2_port = 0x82;
+    dma_done_mask  = (1 << 3);
+    dma_mask       = 3;
+    dma_base_port  = 6;
+    dma_count_port = 7;
+  } else {
+    dma_mode       = 0x49;
+    dma_addr2_port = 0x83;
+    dma_done_mask  = (1 << 1);
+    dma_mask       = 1;
+    dma_base_port  = 2;
+    dma_count_port = 3;
+  }
+
+  //printf("Initialized DMA on channel %d\n", dma_chan);
+}
+
+void graphics_finalize() { }
+
+#define DMA0_STATUS  0x08
+#define DMA0_MASK    0x0A
+#define DMA0_MODE    0x0B
+#define DMA0_COUNTER 0x0C
+
+void dma_write(byte far* line) {
+  unsigned int seg, off, addr, count;
+
+  outp(DMA0_MODE,    dma_mode);
+  outp(DMA0_COUNTER, 0xFF    );
+
+  seg = FP_SEG(line);
+  off = FP_OFF(line);
+
+  addr = ((seg & 0x0FFF) << 4) + off;
+  outp(dma_base_port ,  addr &   0xFF);
+  outp(dma_base_port , (addr & 0xFF00) >>  8);
+  outp(dma_addr2_port, (seg  & 0xF000) >> 12);
+
+  count = SCANLINE-1;
+  outp(dma_count_port,  count &   0xFF);
+  outp(dma_count_port, (count & 0xFF00) >> 8);
+
+  outp(DMA0_MASK, dma_mask);
+
+  outp(CONFIG_PORT, 0x8f);
+  outp(CONFIG_PORT, 0x0f);
+  outp(CONFIG_PORT,    7);
+}
+
+inline byte dma_finished() {
+  byte v = inp(DMA0_STATUS);
+  return (v & dma_done_mask);
+}
+
+inline void lineaddr_reset() {
+  outp(CONFIG_PORT, 5);
+  outp(CONFIG_PORT, 7);
+}
+
+inline void lineaddr_incr() {
+  outp(CONFIG_PORT, 6);
+  outp(CONFIG_PORT, 7);
+}
+
+void graphics_update() {
+  idx y;
+
+  lineaddr_reset();
+  for (y=0; y<GRID_Y; y++) {
+    dma_write(&led_buf[SCANLINE*y]);
+    while (!dma_finished());
+    lineaddr_incr();
+  }
+}
+
+void grid_set(idx x, idx y, val v) {
+  unsigned short off;
+  byte mask;
+
+  v = v ? 1 : 0;
+
+  grid_w[IDX(x,y)] = v;
+
+  off = SCANLINE*y + (x >> 2);
+  mask = 0xC0 >> ((x&3) << 1);
+  if (v) {
+    led_buf[off] |= mask;
+  } else {
+    led_buf[off] &= ~mask;
+  }
+}
+
+#endif
+
+
+
+
+void grid_flip() {
+  val* tmp;
+  tmp = grid_r;
+  grid_r = grid_w;
+  grid_w = tmp;
+
+  graphics_update();
+}
+
+void grid_init() {
   idx x,y;
   for (y=0; y<GRID_Y; y++) {
     for (x=0; x<GRID_X; x++) {
-      grid_set(x, y, rand_byte() % 2);
+      grid_set(x, y, rand_byte() & 1);
     }
   }
   grid_flip();
 }
 
-#define SET_ALIVE(x,y) grid_set((x), (y), (nghb == 3) || ((nghb == 2) && self))
+// rule selection
+#define B(n) ((1 << (n)))
+// HighLife
+short born = B(3) | B(6);
+short live = B(2) | B(3);
+
+#define SET_ALIVE(x,y) grid_set((x), (y), (self ? live : born) & B(nghb))
 #define GR(x,y) grid_r[IDX((x),(y))]
 
 val glider[9] = { 0, 0, 1, 1, 0, 1, 0, 1, 1 };
@@ -128,30 +279,25 @@ void step() {
   idx x,y,xm,xp,ym,yp;
   val self, nghb;
 
+  // implement wrapping with inversion
+  // for projective-plane geometry
+  // don't wrap the corners
+
   // corners
 
   self = GR(0,0);
-  nghb = GR(MAX_X,MAX_Y) + GR(0,MAX_Y) + GR(1,MAX_Y)
-       + GR(MAX_X,    0)               + GR(1,    0)
-       + GR(MAX_X,    1) + GR(0,    1) + GR(1,    1);
+  nghb = GR(1,0) + GR(0,1) + GR(1,1);
   SET_ALIVE(0,0);
 
   self = GR(MAX_X, 0);
-  nghb = GR(MAX_X-1,MAX_Y) + GR(MAX_X,MAX_Y) + GR(0,MAX_Y)
-       + GR(MAX_X-1,    0)                   + GR(0,    0)
-       + GR(MAX_X-1,    1) + GR(MAX_X,    1) + GR(0,    1);
-  SET_ALIVE(MAX_X, 0);
+  nghb = GR(MAX_X-1, 0) + GR(MAX_X-1, 1) + GR(MAX_X, 1);
 
   self = GR(0, MAX_Y);
-  nghb = GR(MAX_X, MAX_Y-1) + GR(0, MAX_Y-1) + GR(1, MAX_Y-1)
-       + GR(MAX_X,   MAX_Y)                  + GR(1,   MAX_Y)
-       + GR(MAX_X,       0) + GR(0,       0) + GR(1,       0);
+  nghb = GR(0, MAX_Y-1) + GR(1, MAX_Y-1) + GR(1, MAX_Y);
   SET_ALIVE(0, MAX_Y);
 
   self = GR(MAX_X, MAX_Y);
-  nghb = GR(MAX_X-1, MAX_Y-1) + GR(MAX_X, MAX_Y-1) + GR(0, MAX_Y-1)
-       + GR(MAX_X-1,   MAX_Y)                      + GR(0,   MAX_Y)
-       + GR(MAX_X-1,       0) + GR(MAX_X,       0) + GR(0,       0);
+  nghb = GR(MAX_X-1, MAX_Y-1) + GR(MAX_X, MAX_Y-1) + GR(MAX_X-1, MAX_Y);
   SET_ALIVE(MAX_X, MAX_Y);
 
   // left and right borders
@@ -160,15 +306,15 @@ void step() {
     yp = y+1;
 
     self = GR(0,y);
-    nghb = GR(MAX_X, ym) + GR(0, ym) + GR(1, ym)
-         + GR(MAX_X,  y)             + GR(1,  y)
-         + GR(MAX_X, yp) + GR(0, yp) + GR(1, yp);
+    nghb = GR(MAX_X, MAX_Y-ym) + GR(0, ym) + GR(1, ym)
+         + GR(MAX_X, MAX_Y- y)             + GR(1,  y)
+         + GR(MAX_X, MAX_Y-yp) + GR(0, yp) + GR(1, yp);
     SET_ALIVE(0,y);
 
     self = GR(MAX_X,y);
-    nghb = GR(MAX_X-1, ym) + GR(MAX_X, ym) + GR(0, ym)
-         + GR(MAX_X-1,  y)                 + GR(0,  y)
-         + GR(MAX_X-1, yp) + GR(MAX_X, yp) + GR(0, yp);
+    nghb = GR(MAX_X-1, ym) + GR(MAX_X, ym) + GR(0, MAX_Y-ym)
+         + GR(MAX_X-1,  y)                 + GR(0, MAX_Y- y)
+         + GR(MAX_X-1, yp) + GR(MAX_X, yp) + GR(0, MAX_Y-yp);
     SET_ALIVE(MAX_X,y);
   }
 
@@ -178,7 +324,7 @@ void step() {
     xp = x+1;
 
     self = GR(x,0);
-    nghb = GR(xm, MAX_Y) + GR(x, MAX_Y) + GR(xp, MAX_Y)
+    nghb = GR(MAX_X-xm, MAX_Y) + GR(MAX_X-x, MAX_Y) + GR(MAX_X-xp, MAX_Y)
          + GR(xm,     0)                + GR(xp,     0)
          + GR(xm,     1) + GR(x,     1) + GR(xp,     1);
     SET_ALIVE(x,0);
@@ -186,7 +332,7 @@ void step() {
     self = GR(x,MAX_Y);
     nghb = GR(xm, MAX_Y-1) + GR(x, MAX_Y-1) + GR(xp, MAX_Y-1)
          + GR(xm,   MAX_Y)                  + GR(xp,   MAX_Y)
-         + GR(xm,       0) + GR(x,       0) + GR(xp,       0);
+         + GR(MAX_X-xm,       0) + GR(MAX_X-x,       0) + GR(MAX_X-xp,       0);
     SET_ALIVE(x,MAX_Y);
   }
 
@@ -206,21 +352,19 @@ void step() {
     }
   }
 
-  if (rand_byte() < 16) spawn_glider();
+  if (rand_byte() < 4) spawn_glider();
 
   grid_flip();
 }
 
 int main() {
-  jvf_init();
   srand(time(NULL));
-  _setvideomode(_MRES4COLOR);
-  draw_border();
-  grid_seed();
+  graphics_init();
+  grid_init();
   for (;;) {
     step();
     if (kbhit()) break;
   }
-  _setvideomode(_DEFAULTMODE);
+  graphics_finalize();
   return 0;
 }
